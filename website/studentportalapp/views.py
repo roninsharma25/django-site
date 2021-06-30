b49f3a80d8b200b4edb70a6a8bbb329af22ab1e9
Ronin Sharma <rrs234@cornell.edu>
2021-06-29 23:52:51 -0400

    updated student portal page

--- website/studentportalapp/views.py
+++ website/studentportalapp/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'studentPortal.html')
 
 def index(request):
     return HttpResponse("Hello, world. You're at the portal index.")
