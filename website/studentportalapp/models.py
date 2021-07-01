bed4e2079fc24dbfa4d10794e9ae1dd2c5589a88
Ronin Sharma <rrs234@cornell.edu>
2021-07-01 01:27:47 -0400

    added sqlite database with example entries

--- website/studentportalapp/models.py
+++ website/studentportalapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    studentName = models.CharField(max_length = 50)
+    studentAge = models.IntegerField(default = 0)
+
+    # Other fields
+    # Availability
+    # Instructor
+    # Tutoring subject
+
+    def __str__(self):
+        return self.studentName + " is age " + str(self.studentAge)
+
+    def getName(self):
+        return self.studentName
+    
+    def getAge(self):
+        return self.studentAge
+
--- website/studentportalapp/views.py
+++ website/studentportalapp/views.py
@@ -1,8 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . import models
 
 def home(request):
     return render(request, 'studentPortal.html')
+
+def test(request):
+    name = request.GET.get('name')
+    if name is not None:
+        command = 'SELECT * from studentportalapp_student WHERE studentName = ' + f'"{name}"'
+        names = []
+        results = models.Student.objects.raw(command)
+        if len(results) > 0:
+            for s in results:
+                names.append(s.studentName)
+            return HttpResponse(names)
+        else:
+            return HttpResponse("No students with that name!")
+    else:
+        return HttpResponse("Invalid Name!")
 
 def index(request):
     return HttpResponse("Hello, world. You're at the portal index.")
